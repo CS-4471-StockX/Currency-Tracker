097e49ed7e37d560950e245c2de8f4c9e2fe322f
wanger26 <jwanger@uwo.ca>
2021-12-09 09:22:34 -0500

    Refactored code to remove creds

--- src/main/java/com/stockx/currencytracker/configurations/AppConfiguration.java
+++ src/main/java/com/stockx/currencytracker/configurations/AppConfiguration.java
@@ -1,6 +1,7 @@
 package com.stockx.currencytracker.configurations;
 
 import com.stockx.currencytracker.adapters.CurrencyConverterAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +9,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfiguration {
 
+    @Value("${currconv.apiKey}")
+    private String currconvApiKey;
+
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
 
     @Bean
-    public CurrencyConverterAdapter currencyConverterAdapter(){
-        return new CurrencyConverterAdapter();
+    public CurrencyConverterAdapter currencyConverterAdapter(RestTemplate restTemplate) {
+        return new CurrencyConverterAdapter(currconvApiKey, restTemplate);
     }
 
 }
